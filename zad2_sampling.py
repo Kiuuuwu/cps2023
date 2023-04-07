02b7787174b25bd62620200424504b351239e134
Michal Banasiak <bananiwo@gmail.com>
2023-04-07 14:32:31 +0200

    ZOH reconstruction

--- zad2_sampling.py
+++ zad2_sampling.py
@@ -16,7 +16,7 @@ def constant_sampling():
     main.draw_graph("Constant sampling", time_start, time_end, basic_freq * (time_end - time_start) / fs, samples_num, signal_sampled)
 
 
-def constant_quantization_with_clip():
+def constant_quantization_with_clip(if_draw=True):
     time_start, time_end, basic_freq = user_communication.get_data();
     signal, start, end = main.sinus_signal(basic_freq, time_start, time_end, False)
     clip_value = user_communication.get_clip_value()
@@ -27,17 +27,20 @@ def constant_quantization_with_clip():
         v1 = int(original_value - v)
         new_signal[x] = v1
 
-    t = np.linspace(int(time_start), int(time_end), int(basic_freq * (time_end - time_start)))
-    fig, ax = plt.subplots()
-    ax.plot(t, signal, label="original")
-    ax.plot(t, new_signal, label="quantizied")
-    ax.set_xlabel('Time (s)')
-    ax.set_ylabel('Amplitude')
-    # ax.legend()
-    plt.show()
+    if if_draw:
+        t = np.linspace(int(time_start), int(time_end), int(basic_freq * (time_end - time_start)))
+        fig, ax = plt.subplots()
+        ax.plot(t, signal, label="original")
+        ax.plot(t, new_signal, label="quantizied")
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Amplitude')
+        ax.legend()
+        plt.show()
+
+    return time_start, time_end, new_signal, basic_freq
 
 
-def constant_quantization_with_rounding():
+def constant_quantization_with_rounding(if_draw):
     time_start, time_end, basic_freq = user_communication.get_data();
     signal, start, end = main.sinus_signal(basic_freq, time_start, time_end, False)
     clip_value = user_communication.get_clip_value()
@@ -46,12 +49,41 @@ def constant_quantization_with_rounding():
         new_signal[x] = round(signal[x], clip_value)
 
     t = np.linspace(int(time_start), int(time_end), int(basic_freq * (time_end - time_start)))
+    if if_draw:
+        fig, ax = plt.subplots()
+        ax.plot(t, signal, label="original")
+        ax.plot(t, new_signal, label="quantizied")
+        ax.set_xlabel('Time (s)')
+        ax.set_ylabel('Amplitude')
+        ax.legend()
+        plt.show()
+
+    return time_start, time_end, new_signal, basic_freq
+
+
+def zero_order_hold_reconstruction():
+    #todo: rozjezdza sie
+    time_start, time_end, signal, original_freq = constant_quantization_with_clip(if_draw=False)
+    frequency = 50
+    new_signal = np.zeros(frequency * int(time_end - time_start))
+    original_samples_num = len(signal)
+    new_samples_num = len(new_signal)
+
+    ratio = int(new_samples_num / original_samples_num)
+    for x in range(original_samples_num):
+        print(signal[x])
+        for y in range(ratio):
+            new_signal[x * ratio + y] = copy.deepcopy(signal[x])
+            print(new_signal[x*ratio + y])
+
+    t1 = np.linspace(int(time_start), int(time_end), int(original_freq * (time_end - time_start)))
+    t2 = np.linspace(int(time_start), int(time_end), int(frequency * (time_end - time_start)))
     fig, ax = plt.subplots()
-    ax.plot(t, signal, label="original")
-    ax.plot(t, new_signal, label="quantizied")
+    ax.plot(t1, signal, label="original")
+    ax.plot(t2, new_signal, label="reconstructed")
     ax.set_xlabel('Time (s)')
     ax.set_ylabel('Amplitude')
-    # ax.legend()
+    ax.legend()
     plt.show()
 
 
@@ -93,6 +125,7 @@ def round(original_value, clip_value):
     return rounded
 
 # test()
-constant_quantization_with_rounding()
+zero_order_hold_reconstruction()
+# constant_quantization_with_rounding(True)
 # constant_quantization_with_clip()
 # constant_sampling()
